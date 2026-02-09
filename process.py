d8bbb54db35e1a098dff7626288537b7299af5a1
seowoo <swchoi1@itcen.com>
2026-02-10 08:18:27 +0900

    fix: pubsub 스키마 수정

--- process.py
+++ process.py
@@ -1,14 +1,14 @@
 import json
 
-def process_message(user_input):
+def process_message(raw_input):
     try:
-        data = json.loads(user_input.data.decode("utf-8"))
+        data = json.loads(raw_input.data.decode("utf-8"))
         user_name = data.get("user_name")
-        number = data.get("number")
+        user_input = data.get("user_input")
 
-        print(f"수신된 요청 : user_name [{user_name}], number [{number}]")
+        print(f"수신된 요청 : user_name [{user_name}], user_input [{user_input}]")
 
-        processed_value = f"{number}000"
+        processed_value = f"{user_input}000"
 
         result_payload = f"{user_name}_{processed_value}"
 
