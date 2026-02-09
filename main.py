9f064e41f268ab213cfafb3e11115271decc6ce6
seowoo <swchoi1@itcen.com>
2026-02-10 08:49:19 +0900

    feat: secret manager 사용

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import os
 import json
-from google.cloud import pubsub_v1
+from google.cloud import pubsub_v1, secret_manager
 
 from process import process_message
 
+def get_secret(secret_id):
+    client = secret_manager.SecretManagerServiceClient()
+    project_id = "agentgo-studio"
+    name=f"projects/{project_id}/{secret_id}/versions/latest"
+
+    try:
+        response = client.access_secret_version()
+        return response.payload.data.decode("utf-8")
+    except Exception as e:
+        print(e)
+        return None
+
 PROJECT_ID = os.getenv("PROJECT_ID")
-INPUT_TOPIC_ID = os.getenv("INPUT_TOPIC_ID")
-OUTPUT_TOPIC_ID = os.getenv("OUTPUT_TOPIC_ID")
+INPUT_TOPIC_ID = get_secret("INPUT_TOPIC_ID")
+OUTPUT_TOPIC_ID = get_secret("OUTPUT_TOPIC_ID")
 
 subscriber = pubsub_v1.SubscriberClient()
 publisher = pubsub_v1.PublisherClient()
